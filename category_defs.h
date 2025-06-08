72a11b4a4a7bc8c198dbc48e8eb8e6c104281a57
RedactedCoder23 <ewrwe24242@gmail.com>
2025-06-08 19:50:51 +1000

    feat: add generate_aos_mappings.py to auto-generate command map and categories

---
+++ category_defs.h
@@ -0,0 +1,8 @@
+// Auto-generated category IDs
+#ifndef CATEGORY_DEFS_H
+#define CATEGORY_DEFS_H
+
+#define CAT_BASIC  0x1
+#define CAT_TIMERS  0x2
+
+#endif
---
+++ commands.c
@@ -0,0 +1,8 @@
+// Auto-generated: CommandDictionary array
+#include "command_interpreter.h"
+
+CommandDictionary commands[] = {
+  { "BASIC_RESET", "1ABC", handler_reset },
+  { "TIMERS_START", "2DEF", handler_timer_start },
+  { NULL, NULL, NULL }
+};
