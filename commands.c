9d7d69b98add1afdc4e101ecddb66805f2917e21
RedactedCoder23 <ewrwe24242@gmail.com>
2025-06-08 23:07:13 +1000

    feat: integrate category IDs into mapping generator and CommandDictionary entries

---
+++ commands.c
@@ -0,0 +1,8 @@
+// Auto-generated: CommandDictionary array
+#include "command_interpreter.h"
+
+CommandDictionary commands[] = {
+  { "HELLO", "010000", say_hello, CAT_HELLO },
+  { "BYE", "020000", say_bye, CAT_BYE },
+  { NULL, NULL, NULL, 0 }
+};
--- include/command_interpreter.h
+++ include/command_interpreter.h
@@ -2,11 +2,14 @@
 #define COMMAND_INTERPRETER_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include "interpreter.h"
+#include "category_defs.h"
 typedef struct {
     const char *name;
     const char *binary;
     void (*handler)();
+    uint8_t category;
 } CommandDictionary;
 
 #endif
