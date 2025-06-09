ab306bb69fc8f8b0a605878324059c1cacdba183
RedactedCoder23 <ewrwe24242@gmail.com>
2025-06-09 21:48:45 +1000

    chore(repo): organize auto-gen code and scripts

---
+++ tests/python/test_generate_mappings.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import tempfile
+import unittest
+
+SCRIPT = os.path.join('scripts', 'generate_aos_mappings.py')
+
+class GenerateMappingsTest(unittest.TestCase):
+    def test_validate(self):
+        res = subprocess.run(['python3', SCRIPT, '--validate'], capture_output=True)
+        self.assertEqual(res.returncode, 0, res.stdout.decode())
+
+    def test_generate_outdir(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            res = subprocess.run(['python3', SCRIPT, '--outdir', tmp], capture_output=True)
+            self.assertEqual(res.returncode, 0, res.stderr.decode())
+            self.assertTrue(os.path.exists(os.path.join(tmp, 'command_map.c')))
+            self.assertTrue(os.path.exists(os.path.join(tmp, 'commands.c')))
+            self.assertTrue(os.path.exists(os.path.join(tmp, 'category_defs.h')))
+
+if __name__ == '__main__':
+    unittest.main()
