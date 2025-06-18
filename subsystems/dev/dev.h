3973880eed0f61a8e0e1764ea0e76d762498cf7e
RedactedCoder23 <ewrwe24242@gmail.com>
2025-06-18 22:06:15 +1000

    dev subsystem registration

--- subsystems/dev/dev.h
+++ subsystems/dev/dev.h
@@ -5,6 +5,16 @@
  */
 #ifndef AOS_DEV_H
 #define AOS_DEV_H
+
+typedef struct device {
+    const char *name;
+    int (*init)(void);
+} Device;
+
 void driver_register(const char *name);
 void driver_list(void);
+void dev_init(void);
+int dev_read(const char *name, void *buf, int len);
+int dev_write(const char *name, const void *buf, int len);
 #endif
+
